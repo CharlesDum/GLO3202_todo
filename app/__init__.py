802b5f49697455e9202a8462db91d1be56fa76a3
Charles-Étienne <charles-etienne.dumont.1@ulaval.ca>
2025-03-11 16:28:17 -0400

    Retirer les données sensibles du fichier .env et utiliser des variables d'environnements système à la place

--- app/__init__.py
+++ app/__init__.py
@@ -8,17 +8,19 @@ from app.models import User
 
 mongo = PyMongo()
 
-load_dotenv()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-    app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+    
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+    if not app.config["MONGO_URI"] or not app.config["SECRET_KEY"]:
+        raise ValueError("Les variables d'environnement MONGO_URI et SECRET_KEY ne sont pas définies")
 
     mongo.init_app(app)
     login_manager.init_app(app)
-    
     login_manager.login_view = "login"
 
     return app
